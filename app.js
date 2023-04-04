a99becc7db983cde83c86d5135b16136d68ed918
DESKTOP-N3RMADT\김태훈 <niceman555@gmail.com>
2023-04-04 18:41:30 +0900

    최초작성테스트

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const dotenv = require('dotenv')
+const path = require("path");
+
+dotenv.config();
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+// app.get('/', (req, res)=> {
+//     res.send('Hello Express');
+// })
+
+app.use(morgan('dev'))
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly:true,
+        secure:false
+    },
+    name:'session-cookie'
+}))
+
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행됩니다.')
+    next()
+})
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(app.get('port'), ()=> {
+    console.log(app.get('port'), '번 포트에서 대기중');
+})
