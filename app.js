80d87ee26e5bfd48ac078ef6abb7c02a9fa260a9
DESKTOP-N3RMADT\김태훈 <niceman555@gmail.com>
2023-04-05 18:43:34 +0900

    app.js에 설명 추가함

--- app.js
+++ app.js
@@ -13,14 +13,24 @@ app.set('port', process.env.PORT || 3000);
 //     res.send('Hello Express');
 // })
 
-app.use(morgan('dev'))
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'))      //로그를 남김
+
+// 정적 파일제공에 사용함
+// public폴더를 정적파일 제공 폴더로 지정. css나 js나 html, 이미지를 넣으면 브라우져에서 접근가능함
+app.use('/', express.static(path.join(__dirname, 'public')))    
+
+//body-parser 
+//body-parser의 일부기능들이 express에 내장되어 바로 사용가능함
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+// cookie를 해석해 req.cookie객체로 만듬
 app.use(cookieParser(process.env.COOKIE_SECRET))
+
+//세션관리용 미들웨어
 app.use(session({
-    resave:false,
-    saveUninitialized:false,
+    resave:false,       //요청이 올때마다 세션 수정사항이 없어도 세션 다시 저장할지
+    saveUninitialized:false,    //세션에 저장할 내용이 없어도 처음부터 생성할지
     secret:process.env.COOKIE_SECRET,
     cookie:{
         httpOnly:true,
