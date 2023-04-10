6f99f4af309b66e04439b050de5f9118b5acc4e0
niceman555 <niceman555@gmail.com>
2023-04-11 00:03:50 +0900

    upload

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 //body-parser 
 //body-parser의 일부기능들이 express에 내장되어 바로 사용가능함
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended : false }))
 
 // cookie를 해석해 req.cookie객체로 만듬
 app.use(cookieParser(process.env.COOKIE_SECRET))
@@ -39,13 +39,56 @@ app.use(session({
     name:'session-cookie'
 }))
 
-app.use((req, res, next) => {
-    console.log('모든 요청에 실행됩니다.')
-    next()
+// 파일 업로드를 위해 사용함 
+const multer = require("multer")
+const fs = require("fs");
+
+try {
+    fs.readdirSync('uploads')
+} 
+catch (error) {
+    console.error('uploads폴더가 없어 uploads폴더를 생성합니다.')
+    fs.mkdirSync('uploads')
+}
+
+const upload = multer({
+    storage:multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/')
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits : { fileSize : 5 * 1024 * 1024 },    
+});
+app.get('/upload', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'multipart.html'));
+})
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log(req.file)
+    res.send('ok');
 })
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/index.html'))
+// app.use((req, res, next) => {
+//     console.log('모든 요청에 실행됩니다.')
+//     next()
+// })
+
+// app.get('/', (req, res)=> {
+//     res.sendFile(path.join(__dirname, '/index.html'))
+// })
+
+app.get('/', (req, res, next)=> {
+    console.log('GET / 요청에서만 실행됩니다.')
+    next();
+}, (req, res) => {
+    throw new Error("에러는 에러처리 미들웨어로 갑니다.")
+})
+app.use((err, req, res, next) => {
+    console.error(err)
+    res.status(500).send(err.message);
 })
 
 app.listen(app.get('port'), ()=> {
